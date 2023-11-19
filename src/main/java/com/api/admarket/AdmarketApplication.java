cf9252b0cca1bb94e03443482d4f4bbffbe3c6df
Lemone28 <lemone681@gmail.com>
2023-11-19 22:43:46 +0200

    Indexing of the project structure
    - Add .gitkeep files in all empty directory
    - Add pom.xml
    - Add src/main/resources/application.yml
    - Add src/main/java/com/api/admarket/AdmarketApplication.java

---
+++ src/main/java/com/api/admarket/AdmarketApplication.java
@@ -0,0 +1,13 @@
+package com.api.admarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/admarket/AdmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.admarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
