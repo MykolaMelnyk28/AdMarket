285f75fb645cd0025af699b924fbc456d801ee09
MykolaMelnyk28 <lemone681@gmail.com>
2023-11-30 01:11:21 +0200

    Create entity Category of table 'categories'

---
+++ src/main/java/com/api/admarket/api/v1/entity/ad/Category.java
@@ -0,0 +1,29 @@
+package com.api.admarket.api.v1.entity.ad;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "parent_name")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children;
+
+}
