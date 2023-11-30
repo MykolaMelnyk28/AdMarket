b89cca21d6effcf386e641f7bb2705b567361262
MykolaMelnyk28 <lemone681@gmail.com>
2023-11-30 18:27:50 +0200

    Modify SavedAdRepository

--- src/main/java/com/api/admarket/api/v1/repository/SavedAdRepository.java
+++ src/main/java/com/api/admarket/api/v1/repository/SavedAdRepository.java
@@ -4,9 +4,11 @@ import com.api.admarket.api.v1.entity.ad.SavedAd;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SavedAdRepository extends JpaRepository<SavedAd, Long> {
 
-
+    List<SavedAd> findAllByUserId(Long id);
 
 }
