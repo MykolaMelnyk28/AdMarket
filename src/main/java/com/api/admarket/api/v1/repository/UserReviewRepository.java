f3dd3e77bc589b5382cd31009c2a87556f7eb367
MykolaMelnyk28 <lemone681@gmail.com>
2023-11-30 18:52:13 +0200

    Modify UserReviewRepository

--- src/main/java/com/api/admarket/api/v1/repository/UserReviewRepository.java
+++ src/main/java/com/api/admarket/api/v1/repository/UserReviewRepository.java
@@ -2,9 +2,16 @@ package com.api.admarket.api.v1.repository;
 
 import com.api.admarket.api.v1.entity.user.UserReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
+
+    List<UserReview> findAllByReviewerId(Long reviewId);
+    List<UserReview> findAllBySellerId(Long sellerId);
+    List<UserReview> findAllByRatingBetween(int start, int end);
 
 }
