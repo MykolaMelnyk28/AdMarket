1682778a2b67ebf00e4c843d45ce58c1e55b1f6f
MykolaMelnyk28 <lemone681@gmail.com>
2023-12-06 00:25:15 +0200

    Modify ControllerAdvice
    - Create method handle for MethodArgumentNotValidException, ResponseStatus(HttpStatus.BAD_REQUEST)

--- src/main/java/com/api/admarket/api/v1/controller/ControllerAdvice.java
+++ src/main/java/com/api/admarket/api/v1/controller/ControllerAdvice.java
@@ -5,9 +5,16 @@ import com.api.admarket.api.v1.exeption.ExceptionBody;
 import com.api.admarket.api.v1.exeption.ResourceMappingException;
 import com.api.admarket.api.v1.exeption.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -42,6 +49,32 @@ public class ControllerAdvice {
             final AccessDeniedException e
     ) {
         return new ExceptionBody("Access denied.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e
+    ) {
+        ExceptionBody exceptionBody = new ExceptionBody("Validation failed.");
+        List<FieldError> errors = e.getBindingResult().getFieldErrors();
+        List<FieldError> errors = appendPasswordConfirm(e.getBindingResult());
+        exceptionBody.setErrors(errors.stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage))
+        );
+        return exceptionBody;
+    }
+
+    private List<FieldError> appendPasswordConfirm(BindingResult bindingResult) {
+        List<FieldError> passwordConfirmErrors = new ArrayList<>(bindingResult.getFieldErrors());
+
+        for (ObjectError error : bindingResult.getGlobalErrors()) {
+            if (error.getObjectName().equals("userDTO") && error.getCode().equals("PasswordConfirm")) {
+                passwordConfirmErrors.add(new FieldError("userDTO", "passwordConfirm", error.getDefaultMessage()));
+            }
+        }
+
+        return passwordConfirmErrors;
     }
 
     @ExceptionHandler(Exception.class)
