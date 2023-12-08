46f4758c70a754836c3757413f33b37eef36d58d
MykolaMelnyk28 <lemone681@gmail.com>
2023-12-08 10:36:37 +0200

    Temp open access on endpoint

--- src/main/java/com/api/admarket/config/ApplicationConfig.java
+++ src/main/java/com/api/admarket/config/ApplicationConfig.java
@@ -1,8 +1,21 @@
 package com.api.admarket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(request -> {
+            request.anyRequest().permitAll();
+        });
+        return http.build();
+    }
 
 }
