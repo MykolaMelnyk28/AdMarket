591ac60337ae072e785c7d780c90fbab232e47c7
MykolaMelnyk28 <lemone681@gmail.com>
2023-12-10 19:34:09 +0200

    Add security properties in application.yml

--- src/main/java/com/api/admarket/AdmarketApplication.java
+++ src/main/java/com/api/admarket/AdmarketApplication.java
@@ -2,7 +2,11 @@ package com.api.admarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
+@EnableMethodSecurity
 @SpringBootApplication
 public class AdmarketApplication {
 
